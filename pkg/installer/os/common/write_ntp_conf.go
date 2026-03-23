0f62218ce3b9d9698da0a254ee4fdf0e3bdb1340
Stefan Majer <stefan.majer@f-i-ts.de>
2026-03-23 11:24:44 +0100

    Remove duplicate chrony template rendering

--- pkg/installer/os/common/write_ntp_conf.go
+++ pkg/installer/os/common/write_ntp_conf.go
@@ -4,9 +4,6 @@ import (
 	"context"
 	"fmt"
 	"strings"
-
-	apiv2 "github.com/metal-stack/api/go/metalstack/api/v2"
-	"github.com/metal-stack/os-installer/pkg/services/chrony"
 )
 
 const (
@@ -23,29 +20,6 @@ func (d *CommonTasks) WriteNTPConf(ctx context.Context) error {
 
 	for _, ntp := range d.allocation.NtpServers {
 		ntpServers = append(ntpServers, ntp.Address)
-	}
-
-	if d.allocation.AllocationType == apiv2.MachineAllocationType_MACHINE_ALLOCATION_TYPE_FIREWALL {
-		defaultVRF, err := d.network.GetTenantNetworkVrfName()
-		if err != nil {
-			return err
-		}
-
-		// TODO: check if this is really required as chrony also gets set up in systemd service task?
-
-		_, err = chrony.WriteSystemdUnit(ctx, &chrony.Config{
-			Log:    d.log,
-			Reload: false,
-			Enable: true,
-		}, &chrony.TemplateData{
-			NTPServers: ntpServers,
-		}, defaultVRF)
-
-		if err != nil {
-			return err
-		}
-
-		return d.WriteNtpConfToPath(ChronyConfigPath, ntpServers)
 	}
 
 	return d.WriteNtpConfToPath(TimesyncdConfigPath, ntpServers)
